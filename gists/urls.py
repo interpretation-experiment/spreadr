52c4a9b7e204dac7316f2edf71b0af5ddd897c08
Sébastien Lerique <sl@mehho.net>
2014-12-11 19:19:32 +0100

    Use rest_frameworky views

--- gists/urls.py
+++ gists/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from gists import views
 
 
@@ -6,3 +7,6 @@ urlpatterns = [
     url(r'^sentences/$', views.sentence_list),
     url(r'^sentences/(?P<pk>[0-9]+)/$', views.sentence_detail),
 ]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- gists/views.py
+++ gists/views.py
@@ -1,62 +1,49 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from gists.models import Sentence
 from gists.serializers import SentenceSerializer
 
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-@csrf_exempt
-def sentence_list(request):
+@api_view(['GET', 'POST'])
+def sentence_list(request, format=None):
     """
     List all sentences, or create a new sentence.
     """
     if request.method == 'GET':
         sentences = Sentence.objects.all()
         serializer = SentenceSerializer(sentences, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SentenceSerializer(data=data)
+        serializer = SentenceSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def sentence_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def sentence_detail(request, pk, format=None):
     """
     Retrieve, update or delete a sentence.
     """
     try:
         sentence = Sentence.objects.get(pk=pk)
     except Sentence.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = SentenceSerializer(sentence)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SentenceSerializer(sentence, data=data)
+        serializer = SentenceSerializer(sentence, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         sentence.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
