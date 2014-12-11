df75ff5b914eae274d8e50bb51a76f2c3191a208
Sébastien Lerique <sl@mehho.net>
2014-12-11 19:52:53 +0100

    Use mixins

--- gists/views.py
+++ gists/views.py
@@ -1,52 +1,40 @@
 from gists.models import Sentence
 from gists.serializers import SentenceSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 
 
-class SentenceList(APIView):
+class SentenceList(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   generics.GenericAPIView):
     """
     List all sentences, or create a new sentence.
     """
-    def get(self, request, format=None):
-        sentences = Sentence.objects.all()
-        serializer = SentenceSerializer(sentences, many=True)
-        return Response(serializer.data)
+    queryset = Sentence.objects.all()
+    serializer_class = SentenceSerializer
 
-    def post(self, request, format=None):
-        serializer = SentenceSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class SentenceDetail(APIView):
+class SentenceDetail(mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     generics.GenericAPIView):
     """
     Retrieve, update or delete a sentence.
     """
-    def get_object(self, pk):
-        try:
-            return Sentence.objects.get(pk=pk)
-        except Sentence.DoesNotExist:
-            raise Http404
+    queryset = Sentence.objects.all()
+    serializer_class = SentenceSerializer
 
-    def get(self, request, pk, format=None):
-        sentence = self.get_object(pk)
-        serializer = SentenceSerializer(sentence)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        sentence = self.get_object(pk)
-        serializer = SentenceSerializer(sentence, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        sentence = self.get_object(pk)
-        sentence.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
