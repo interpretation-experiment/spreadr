1fb1eb04c28011972642f3e6b7ddaf5e76cb4d2a
Sébastien Lerique <sl@mehho.net>
2015-01-15 17:09:21 -0300

    Fix urls

--- spreadr/urls.py
+++ spreadr/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns(
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'spreadr.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
