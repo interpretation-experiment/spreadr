d93e34535d69a94e95e5c862b882aa568735aff3
Sébastien Lerique <sl@mehho.net>
2015-01-15 11:43:08 -0300

    Further use REST generics

--- gists/views.py
+++ gists/views.py
@@ -1,40 +1,19 @@
 from gists.models import Sentence
 from gists.serializers import SentenceSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class SentenceList(mixins.ListModelMixin,
-                   mixins.CreateModelMixin,
-                   generics.GenericAPIView):
+class SentenceList(generics.ListCreateAPIView):
     """
     List all sentences, or create a new sentence.
     """
     queryset = Sentence.objects.all()
     serializer_class = SentenceSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SentenceDetail(mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.DestroyModelMixin,
-                     generics.GenericAPIView):
+class SentenceDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a sentence.
     """
     queryset = Sentence.objects.all()
     serializer_class = SentenceSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
