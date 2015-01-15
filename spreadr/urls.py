8468564c1a484ce8c85bdfabac23e1b6847b9dd6
Sébastien Lerique <sl@mehho.net>
2015-01-15 16:39:41 -0300

    Root url is api/v1

--- spreadr/urls.py
+++ spreadr/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
     # Examples:
     # url(r'^$', 'spreadr.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('gists.urls')),
+    url(r'^api/v1/', include('gists.urls')),
 )
