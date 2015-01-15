0976d6af3f47575a0b7aebe51ca915b0e90a6370
Sébastien Lerique <sl@mehho.net>
2015-01-15 15:26:44 -0300

    Use ViewSets and a router

--- gists/urls.py
+++ gists/urls.py
@@ -1,27 +1,20 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 
 from gists import views
 
 
-urlpatterns = [
-    url(r'^$', views.api_root),
-    url(r'^sentences/$',
-        views.SentenceList.as_view(),
-        name='sentence-list'),
-    url(r'^sentences/(?P<pk>[0-9]+)/$',
-        views.SentenceDetail.as_view(),
-        name='sentence-detail'),
-    url(r'^users/$',
-        views.UserList.as_view(),
-        name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$',
-        views.UserDetail.as_view(),
-        name='user-detail'),
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'sentences', views.SentenceViewSet)
+router.register(r'users', views.UserViewSet)
 
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 ]
 
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+#urlpatterns = format_suffix_patterns(urlpatterns)
--- gists/views.py
+++ gists/views.py
@@ -1,55 +1,27 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
+from rest_framework import permissions, viewsets
 
 from gists.models import Sentence
 from gists.serializers import SentenceSerializer, UserSerializer
 from gists.permissions import IsOwnerOrReadOnly
 
 
-@api_view(('GET',))
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'sentences': reverse('sentence-list', request=request, format=format)
-    })
-
-
-class SentenceList(generics.ListCreateAPIView):
+class SentenceViewSet(viewsets.ModelViewSet):
     """
-    List all sentences, or create a new sentence.
-    """
-    queryset = Sentence.objects.all()
-    serializer_class = SentenceSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class SentenceDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve, update or delete a sentence.
+    Sentence list and detail, unauthenticated read, authenticated read-write.
     """
     queryset = Sentence.objects.all()
     serializer_class = SentenceSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
 
-
-class UserList(generics.ListAPIView):
-    """
-    List all users.
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    Retrieve a user.
+    User list and detail, read-only.
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
