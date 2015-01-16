336eb2d3ccaa5cdbaecfddcb54dd4adfe99b4346
Sébastien Lerique <sl@mehho.net>
2015-01-15 21:45:34 -0300

    Add a /users/me/ route

--- gists/views.py
+++ gists/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions, viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from gists.models import Sentence
 from gists.serializers import SentenceSerializer, UserSerializer
@@ -25,3 +28,8 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @list_route(permission_classes=[IsAuthenticated])
+    def me(self, request, format=None):
+        serializer = UserSerializer(request.user, context={'request': request})
+        return Response(serializer.data)
