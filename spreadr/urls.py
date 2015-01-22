d3b401cda3dbab63f340a3ec734c4888b7ce04a1
Sébastien Lerique <sl@mehho.net>
2015-01-22 12:23:18 -0300

    In fact, prefix with /api

--- spreadr/urls.py
+++ spreadr/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('gists.urls')),
+    url(r'^api/', include('gists.urls')),
 )
