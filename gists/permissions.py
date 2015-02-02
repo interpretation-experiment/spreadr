4bde78cb99a427e68e567d50b0aa755f3d3f607b
Sébastien Lerique <sl@mehho.net>
2015-02-02 15:09:03 -0300

    Minor fix

--- gists/permissions.py
+++ gists/permissions.py
@@ -61,6 +61,7 @@ class IsAuthenticatedProfile(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.user.is_authenticated()
+                and hasattr(request.user, 'profile')
                 and request.user.profile is not None)
 
 
