e7c6edc8776dda9abae4876ab0560418a12e5635
Sébastien Lerique <sl@mehho.net>
2015-02-09 18:14:26 -0300

    Add insecure production settings for demo

---
+++ spreadr/settings_prod_insecure.py
@@ -0,0 +1,21 @@
+"""
+Insecure production Django settings for spreadr project.
+"""
+
+# Import default settings
+from spreadr.settings import *
+
+# Secret key
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# No debug
+DEBUG = False
+
+# Hosts
+ALLOWED_HOSTS = [
+    '.gistr.io'
+]
+
+# Static files
+STATIC_ROOT = 'static'
