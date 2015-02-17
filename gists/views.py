2788e10111a4e07942d174b1795dee7f4f08130e
Sébastien Lerique <sl@mehho.net>
2015-02-17 17:53:04 -0300

    Streamline ordering

--- gists/views.py
+++ gists/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db.models import Count
 from django.core.exceptions import PermissionDenied
 from django.conf import settings
-from rest_framework import viewsets, mixins, filters, generics
+from rest_framework import viewsets, mixins, generics
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -55,9 +55,6 @@ class SentenceViewSet(mixins.CreateModelMixin,
     queryset = Sentence.objects.all()
     serializer_class = SentenceSerializer
     permission_classes = (IsAuthenticatedWithProfileOrReadOnly,)
-    filter_backends = (filters.OrderingFilter,
-                       filters.SearchFilter,
-                       SampleFilterBackend,)
     ordering = ('-created',)
     ordering_fields = ('created',)
     search_fields = ('text',)
@@ -104,9 +101,7 @@ class ProfileViewSet(mixins.CreateModelMixin,
     serializer_class = ProfileSerializer
     permission_classes = (IsAuthenticatedWithoutProfileOrReadOnly,
                           IsAdminOrObjectHasSelfOrReadOnly,)
-    #ordering = ('user__username',)
-    #ordering_fields =
-    #search_fields =
+    ordering = ('user__username',)
 
     @list_route(permission_classes=[IsAuthenticatedWithProfile])
     def me(self, request, format=None):
@@ -129,8 +124,6 @@ class UserViewSet(mixins.RetrieveModelMixin,
     serializer_class = UserSerializer
     permission_classes = (IsAdminOrReadOnly,)
     ordering = ('username',)
-    ordering_fields = ('username',)
-    search_fields = ('username',)
 
     @list_route(permission_classes=[IsAuthenticated])
     def me(self, request, format=None):
