99826272b121b91d27a6ffc13433cfd68c64898f
Sébastien Lerique <sl@mehho.net>
2015-05-21 18:32:36 +0200

    Properly configure the 'sites framework'

--- spreadr/settings.py
+++ spreadr/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'rest_framework',
     'rest_framework.authtoken',
@@ -150,3 +151,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Site ID, for the 'sites framework' to work, used by django-allauth
+SITE_ID = 1
