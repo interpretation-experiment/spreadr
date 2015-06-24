e7736197820b633a81b25fda3c836beab0aa7bcd
Sébastien Lerique <sl@mehho.net>
2015-06-25 01:38:27 +0200

    Add email settings

--- spreadr/settings_prod.py
+++ spreadr/settings_prod.py
@@ -33,5 +33,13 @@ DATABASES = {
     }
 }
 
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_USE_TLS = True
+EMAIL_SUBJECT_PREFIX = '[Gistr.io Staff] '
+
 # Static files
 STATIC_ROOT = 'static'
