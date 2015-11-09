fcc936bad7702992ae3ce594ff636ff32a732bfd
Sébastien Lerique <sl@mehho.net>
2015-11-09 12:16:00 +0100

    Add settings for local exploration of data

---
+++ spreadr/settings_home.py
@@ -0,0 +1,24 @@
+"""
+Django settings for spreadr project for home exploration of the data.
+"""
+
+# Import default settings
+from spreadr.settings import *
+
+# Hosts
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '.gistr.io'
+]
+
+# Database
+CONN_MAX_AGE = 300
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.environ['MY_CNF']
+        },
+    }
+}
