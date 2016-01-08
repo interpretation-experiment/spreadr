319891b0ef892a4af3e01e84e1f3d1bdab795d2d
Sébastien Lerique <sl@mehho.net>
2016-01-08 18:10:01 +0100

    Fix pagination for new API in DRF

--- spreadr/settings.py
+++ spreadr/settings.py
@@ -94,9 +94,7 @@ TEMPLATES = [
 # REST Settings
 
 REST_FRAMEWORK = {
-    'PAGINATE_BY': 10,
-    'PAGINATE_BY_PARAM': 'page_size',
-    'MAX_PAGINATE_BY': 100,
+    'DEFAULT_PAGINATION_CLASS': 'spreadr.pagination.SpreadrPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
