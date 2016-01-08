d9992d534c370812457790ef7831a16f7509f2bc
Sébastien Lerique <sl@mehho.net>
2016-01-08 16:18:17 +0100

    Upgrade settings with new TEMPLATES

--- spreadr/settings.py
+++ spreadr/settings.py
@@ -22,8 +22,6 @@ SECRET_KEY = 'ovn7%xtw=_$w4qaqj$9y18)^s-boet5vk*v$3at+oq)o_e06n('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -46,6 +44,7 @@ INSTALLED_APPS = (
 
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
     'rest_auth.registration',
 
     'raven.contrib.django.raven_compat',
@@ -70,7 +69,26 @@ ROOT_URLCONF = 'spreadr.urls'
 
 WSGI_APPLICATION = 'spreadr.wsgi.application'
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 
 # REST Settings
