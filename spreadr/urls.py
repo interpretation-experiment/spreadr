e9926f202506b77ad2c92e6b77803f023969114f
Sébastien Lerique <sl@mehho.net>
2016-01-08 16:32:06 +0100

    Don't use `patterns` anymore, deprecated

--- spreadr/urls.py
+++ spreadr/urls.py
@@ -1,12 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'spreadr.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('glue.urls')),
     url(r'^api/', include('gists.urls')),
-)
+]
